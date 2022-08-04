5943191c6b3419363eda5f08a8b37717da785c16
Daria Kriukova <daria.kriukova@elfys.fi>
2022-08-04 08:01:05 +0300

    add CLI command, new `click` requirement

--- analyzing.py
+++ analyzing.py
@@ -1,9 +1,20 @@
+from typing import Union
+
+import click
 import keyring
 import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Query
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker, Query, Session
 
 from orm import IVMeasurement, Wafer, Chip
+
+engine = create_engine('mysql://{user}:{pwd}@{server}:3306/{db}'.format(
+    **{
+        "user": keyring.get_password("ELFYS_DB", "USER"),
+        "pwd": keyring.get_password("ELFYS_DB", "PASSWORD"),
+        "server": "95.217.222.91",
+        "db": "elfys"
+    }))
 
 
 def get_summary_data(query: Query, voltages: list[float, ...]) -> pd.DataFrame:
@@ -13,7 +24,7 @@ def get_summary_data(query: Query, voltages: list[float, ...]) -> pd.DataFrame:
         s = pd.Series({
             measurement.chip.name: measurement.anode_current_corrected
             for measurement in measurements
-        })
+        }, dtype='float64')
         df.loc[:, voltage] = s
     return df
 
@@ -27,28 +38,36 @@ def get_all_data(query: Query) -> pd.DataFrame:
     return df
 
 
-def main():
-    engine = create_engine('mysql://{user}:{pwd}@{server}:3306/{db}'.format(
-        **{
-            "user": keyring.get_password("ELFYS_DB", "USER"),
-            "pwd": keyring.get_password("ELFYS_DB", "PASSWORD"),
-            "server": "95.217.222.91",
-            "db": "elfys"
-        }))
+@click.command()
+@click.pass_context
+@click.option("--chips-type", help="Type of the chips to analyze.")
+@click.option("-o", "--output", "filename", default="summary.xlsx",
+              help="Output file name. Default: summary.xlsx")
+def analyzing(ctx: click.Context, chips_type: Union[str, None], wafer_name: str, filename: str):
+    session: Session = ctx.obj['session']
+    wafer = session.query(Wafer).filter_by(name=wafer_name).one()
+    query = session.query(IVMeasurement) \
+        .filter(IVMeasurement.chip.has(Chip.wafer.__eq__(wafer)))
+    if chips_type is not None:
+        query = query.filter(IVMeasurement.chip.has(Chip.type.__eq__(chips_type)))
 
-    with sessionmaker(bind=engine).begin() as session:
-        wafer = session.query(Wafer).filter_by(name='BP6').one()
-        query = session.query(IVMeasurement) \
-            .filter(IVMeasurement.chip.has(type='U'),
-                    IVMeasurement.chip.has(Chip.wafer.__eq__(wafer)))
+    summary_data = get_summary_data(query, [0.01, 5, 10, 20, -1])
+    all_data = get_all_data(query)
 
-        summary_data = get_summary_data(query, [0.01, 5, 10, 20, -1])
-        all_data = get_all_data(query)
-
-    with pd.ExcelWriter('output.xlsx') as writer:
+    with pd.ExcelWriter(filename) as writer:
         summary_data.to_excel(writer, sheet_name='Summary')
         all_data.to_excel(writer, sheet_name='All')
         writer.save()
+
+
+def main():
+    with sessionmaker(bind=engine).begin() as session:
+        last_wafer = session.query(Wafer).order_by(desc(Wafer.created_at)).first()
+        default_wafer_name = last_wafer.name
+        wafer_option = click.Option(("-w", "--wafer", "wafer_name"), default=default_wafer_name,
+                                    prompt=f"Wafer name", help="Wafer name.")
+        analyzing.params.append(wafer_option)
+        analyzing(obj={'session': session})
 
 
 if __name__ == '__main__':
