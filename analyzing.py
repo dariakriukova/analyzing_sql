8452cd6d44a087197a5c88ec8a99efea1289b24d
Daria Kriukova <daria.kriukova@elfys.fi>
2022-08-18 07:39:59 +0300

    set matplotlib cache dir and sentry environment

--- analyzing.py
+++ analyzing.py
@@ -1,11 +1,21 @@
+import os
+import pathlib
+import sys
+
 import sentry_sdk
 
-from commands import main
+frozen = getattr(sys, 'frozen', False)
 
 sentry_sdk.init(
     dsn="https://11a7db21d98f44e8b5c66bf16c463149@o121277.ingest.sentry.io/6630216",
-    traces_sample_rate=1.0
+    traces_sample_rate=1.0,
+    environment="pyinstaller" if frozen else "development" if os.getenv("DEV", False) else "pipenv",
 )
 
+if frozen:
+    os.environ['MPLCONFIGDIR'] = str(pathlib.Path(__file__).parent / 'matplotlib' / 'appdata')
+
+
 if __name__ == '__main__':
+    from commands import main
     main()
--- measure.py
+++ measure.py
@@ -1,11 +1,14 @@
-import sentry_sdk
+import os
 
-from commands.measure import measure
+import sentry_sdk
 
 sentry_sdk.init(
     dsn="https://11a7db21d98f44e8b5c66bf16c463149@o121277.ingest.sentry.io/6630216",
-    traces_sample_rate=1.0
+    traces_sample_rate=1.0,
+    environment="development" if os.getenv("DEV", False) else "pipenv",
 )
+
+from commands.measure import measure
 
 if __name__ == '__main__':
     measure()
