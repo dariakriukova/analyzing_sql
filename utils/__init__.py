129c021c1613aaae6d653c44381437bd6100b23c
Daria Kriukova <daria.kriukova@elfys.fi>
2022-09-18 13:47:36 +0300

    fix flatten_options import

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .logger import logger
 from .get_db_url import get_db_url
 from .csv_choice import CsvChoice
 from .thresholds import iv_thresholds, cv_thresholds
+from .flatten_options import flatten_options
